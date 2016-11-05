8140319dbe055ded1de6c8513aa90a820961279f
Hey!Alfred <yu.gao.whut@gmail.com>
2016-11-05 13:04:24 -0700

    Add predict feature for NN made by BJ

--- script/solution.py
+++ script/solution.py
@@ -32,13 +32,21 @@ if __name__ == "__main__":
     MF = MX_F()
     MF.load_data('../data/train_hash_shuff.txt', '\t')
     start_time = time.clock()
-    MF.factorize(k = 40, iter= 500, alpha = 0.0015, beta = 0.05)
+    MF.factorize(k = 2, iter= 1, alpha = 0.0015, beta = 0.05)
+    #TODO Implement save_model function
+    #MF.save_model()
     end_time = time.clock()
 
     result_path = '../data/result_'  + '.txt'
+    NN_CF_Feature = '../data/NN_CF_Feature.txt'
+
     result_file = open(result_path,'w')
+    NN_CF_Feature_file = open(NN_CF_Feature,'w')
     vali_file = open('../data/validate_nolabel.txt','r')
+    train_file = open('../data/invited_info_NN_bj.txt','r')
+
     result_file.write('qid,uid,label\n')
+    NN_CF_Feature_file.write('qid,uid,feature_CF\n')
     is_head = True
     for fileline in vali_file:
         if is_head:
@@ -46,17 +54,14 @@ if __name__ == "__main__":
             continue
         else:
             fileline = fileline.strip('\n\r').split(',')
-            # print fileline
             q_id = fileline[0]
             u_id = fileline[1]
-            # print dict_u[u_id]
-            # print dict_q[q_id]
+
             try:
                 score = MF.predict(dict_u[u_id], dict_q[q_id])
-            # print score
             except Exception as e:
-                score = 0.5
-            print q_id + ',' + u_id + " Unable to predict"
+                score = 0.
+                print q_id + ',' + u_id + " Unable to predict"
 
             if score < 1e-5:
                 score = 0.
@@ -64,3 +69,28 @@ if __name__ == "__main__":
             result = q_id + "," + u_id + "," + str(score) + '\n'
             result_file.write(result)
     result_file.close()
+
+    print "Constructing NN Feature....... :)"
+    is_head = True
+    for fileline in train_file:
+        if is_head:
+            is_head = False
+            continue
+        else:
+            fileline = fileline.strip('\n\r').split('\t')
+            q_id = fileline[0]
+            u_id = fileline[1]
+
+
+            try:
+                score = MF.predict(dict_u[u_id],dict_q[q_id])
+            except Exception as e:
+                score = 0.
+                print q_id + ',' + u_id + " Unable to predict"
+
+            if score < 1e-5:
+                score = 0.
+
+            result = q_id + "," + u_id + "," + str(score) + '\n'
+            NN_CF_Feature_file.write(result)
+    NN_CF_Feature_file.close()
